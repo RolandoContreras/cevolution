7684f16d9d8faa9d0fb9ea38a4480112e9540193
software.contreras@gmail.com <software.contreras@gmail.com>
2017-12-07 22:38:37 -0500

    changes on all page

---
+++ application/views/footer.php
@@ -0,0 +1,102 @@
+<footer id="footer" role="contentinfo" class="site-footer main-footer footer-bottom-horizontal">
+    <div class="container">
+	<div class="footer-widgets footer-collapsed-mobile">
+            <a href="#" class="footer-collapse-link">
+                <span>.</span>
+                <span>.</span>
+                <span>.</span>
+            </a>
+		<div class="footer--widgets widget-area widgets--columned-layout widgets--columns-5" role="complementary">
+			<div id="nav_menu-2" class="widget widget_nav_menu">
+                            <h2 class="widgettitle">Mapa</h2>
+                            <div class="menu-basic-container">
+                                <ul id="menu-basic" class="menu">
+                                    <li id="menu-item-692" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-692"><a href="<?php echo site_url().'home';?>">Inicio</a></li>
+                                    <li id="menu-item-695" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-695"><a href="<?php echo site_url().'about';?>">Acerca</a></li>
+                                    <li id="menu-item-691" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-691"><a href="<?php echo site_url().'plan';?>">Plan</a></li>
+                                    <li id="menu-item-694" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-694"><a href="<?php echo site_url().'contact';?>">Contacto</a></li>
+                                    <li id="menu-item-693" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-693"><a href="<?php echo site_url().'login';?>">Login</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    <div id="nav_menu-3" class="widget widget_nav_menu">
+                        <h2 class="widgettitle">Plan</h2>
+                            <div class="menu-destinations-container">
+                                <ul id="menu-destinations" class="menu">
+                                    <li id="menu-item-143" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-143"><a href="<?php echo site_url().'plan/basic';?>">Basic</a></li>
+                                    <li id="menu-item-144" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-144"><a href="<?php echo site_url().'plan/executive';?>">Executive</a></li>
+                                    <li id="menu-item-144" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-144"><a href="<?php echo site_url().'plan/investor';?>">Investor</a></li>
+                                    <li id="menu-item-145" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-145"><a href="<?php echo site_url().'plan/business';?>">Business</a></li>
+                                    <li id="menu-item-146" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-146"><a href="<?php echo site_url().'plan/master';?>">Master</a></li>
+                                </ul>
+                            </div>
+                    </div>
+                    <div id="nav_menu-4" class="widget widget_nav_menu">
+                        <h2 class="widgettitle">Noticias</h2>
+                            <div class="menu-tickets-container">
+                                <ul id="menu-tickets" class="menu">
+                                    <li id="menu-item-147" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-147"><a href="#">Bitcoin la nueva realidad</a></li>
+                                    <li id="menu-item-156" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-156"><a href="#">Blockchain - Cadena de bloque</a></li>
+                                    <li id="menu-item-148" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-148"><a href="#"><?php echo replace_vocales_voculeshtml("¿Qué es la criptomoneda? - Bitcoin");?></a></li>
+                                </ul>
+                            </div>
+                    </div>
+                    <div id="nav_menu-5" class="widget widget_nav_menu">
+                        <h2 class="widgettitle">Acerca</h2>
+                            <div class="menu-tourism-container">
+                                <ul id="menu-tourism" class="menu">
+                                    <li id="menu-item-152" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-152"><a href="<?php echo site_url().'about';?>"><?php echo corta_texto(replace_vocales_voculeshtml('Multiplataforma virtual que brinda distintos servicios y productos, que utilizando el sistema de mercadeo en red nos permite fidelizar a consumidores potenciales a la marca, de esta forma pudiendo diversificar nuestro activo más importante (clientes) en masa crítica a otras distintas empresas.'),120);?></a></li>
+                                </ul>
+                            </div>
+                    </div>
+                    <div id="nav_menu-6" class="widget widget_nav_menu">
+                        <h2 class="widgettitle"><?php echo replace_vocales_voculeshtml("Información");?></h2>
+                            <div class="menu-information-container">
+                                <ul id="menu-information" class="menu">
+                                    <li id="menu-item-158" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-158"><a href="<?php echo site_url().'about';?>"><?php echo replace_vocales_voculeshtml("Visión");?></a></li>
+                                    <li id="menu-item-159" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-159"><a href="<?php echo site_url().'about';?>"><?php echo replace_vocales_voculeshtml("Misión");?></a></li>
+                                    <li id="menu-item-159" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-159"><a href="<?php echo site_url().'about';?>"><?php echo replace_vocales_voculeshtml("¿Porqué Nosotros?");?></a></li>
+                                    <li id="menu-item-159" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-159"><a href="<?php echo site_url().'about';?>"><?php echo replace_vocales_voculeshtml("Momento");?></a></li>
+                                </ul>
+                            </div>
+                    </div>			
+		</div>
+	</div>
+	<hr>
+    </div>
+	<div class="footer-bottom">
+		<div class="container">
+			<div class="footer-bottom-content">
+					<div class="footer-content-right">
+                                            <ul class="social-networks rounded colored">
+                                                <li>
+                                                    <a href="#" target="_blank" class="facebook" title="Facebook">
+                                                        <i class="fa fa-facebook"></i><span class="name">Facebook</span>
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#" target="_blank" class="twitter" title="Twitter">
+                                                        <i class="fa fa-twitter"></i><span class="name">Twitter</span>
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#" target="_blank" class="instagram" title="Instagram">
+                                                        <i class="fa fa-instagram"></i><span class="name">Instagram</span>
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#" target="_blank" class="youtube" title="YouTube"><i class="fa fa-youtube-play"></i>
+                                                        <span class="name">YouTube</span>
+                                                    </a>
+                                                </li>
+                                            </ul>							
+					</div>
+					<div class="footer-content-left">
+						<div class="copyrights site-info">
+							<p>© Copyright 2017. All Rights Reserved - 3T Company</p>
+						</div>
+					</div>
+				</div>
+		</div>
+	</div>
+</footer>
